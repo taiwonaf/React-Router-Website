ea945d72de86fcc9b750c76bac790f0a3f6b9a26
Nafiu Taiwo <nafiutaiwosolomon@gmail.com>
2022-08-29 02:02:36 +0100

    Added responsive hamburger menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import { useState } from 'react'
 
-const navbar = () => {
+const Navbar = () => {
+  const [ham, setHam] = useState(false)
+  console.log(ham)
   return (
     <div className='navbar'>
         <div className="brand">
             <h2>clearMix</h2>
         </div>
-        <div className="nav-items">
+        <div className="nav-items" style={ham ? {display: "block", top: "80px"}: null}>
             <ul>
                 <li><NavLink to={'/'} className={({isActive}) => isActive ? "nav-link active-nav-link": "nav-link"}>Home</NavLink></li>
                 <li><NavLink to={'Testimonials'} className={({isActive}) => isActive ? "nav-link active-nav-link": "nav-link"}>Testimonial</NavLink></li>
@@ -16,8 +19,13 @@ const navbar = () => {
                 <li><NavLink to={'Schedule'} className={({isActive}) => isActive ? "schedule active-schedule": "schedule"}>Schedule a demo</NavLink></li>
             </ul>
         </div>
+        <div className="hambuger" onClick={() => setHam(!ham)} >
+          <div className="ham ham1"></div>
+          <div className="ham ham2"></div>
+          <div className="ham ham3"></div>
+        </div>
     </div>
   )
 }
 
-export default navbar
+export default Navbar
